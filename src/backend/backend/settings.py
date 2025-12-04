0991417f47821c8642285cad757fffcc00bbdfaf
thong000 <tt0100912@gmail.com>
2025-12-04 09:21:06 +0700

    fix: Thay email gửi OTP bằng Resend thay vì mail cá nhân

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -174,9 +174,10 @@ USE_CLOUD_STORAGE=os.environ.get('USE_CLOUD_STORAGE')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
