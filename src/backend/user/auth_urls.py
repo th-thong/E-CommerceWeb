324fbf641567ce4d153d60acb53300102930de89
thong000 <tt0100912@gmail.com>
2025-12-13 22:23:41 +0700

    feat: Thêm endpoint refresh token

--- src/backend/user/auth_urls.py
+++ src/backend/user/auth_urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/',views.register, name='user_register'),
     path('login/',views.login,name='user_login'),
     path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot_password'),
     path('reset-password/', views.ResetPasswordView.as_view(), name='reset_password'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
