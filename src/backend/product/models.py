b37b194067f10ec2aa44f187d6e47c6f19f9c13f
thong000 <tt0100912@gmail.com>
2025-12-15 10:15:51 +0700

    fix: Sửa lỗi sku đã xóa nhưng vẫn gọi

--- src/backend/product/models.py
+++ src/backend/product/models.py
@@ -47,7 +47,7 @@ class ProductVariant(models.Model):
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.product.product_name} ({self.sku})"
+        return f"{self.product.product_name}"
 
 
 class ProductImage(models.Model):
