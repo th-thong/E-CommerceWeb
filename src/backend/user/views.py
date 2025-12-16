3710c0012525f6dcc71194af089207abed55f569
thong000 <tt0100912@gmail.com>
2025-12-16 23:50:47 +0700

    fix(login): kiểm tra ban trước khi login

--- src/backend/user/views.py
+++ src/backend/user/views.py
@@ -136,6 +136,10 @@ def login(request):
     if user is None or not user.check_password(password):
         return Response({"message" : "Incorrect email or password"}, status = 401)
     
+    if user.status =='banned':
+        return Response({"message" : "Your account has been banned"}, status = 401)
+
+    
     refresh = RefreshToken.for_user(user)
     access_token = str(refresh.access_token)
     refresh_token = str(refresh)
